334a7a4dbf4847b2f9cfaff98a1c82f7dd5f37ec
quh <quhxuxm_quh@163.com>
2018-08-15 17:52:20 +0800

    init

--- root/web/src/app/app.module.ts
+++ root/web/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { TopNavigationComponent } from './top-navigation/top-navigation.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { ArticleCardComponent } from './article-card/article-card.component';
-import { AuthorCardComponent } from './author-card/author-card.component';
-import { AnthologyCardComponent } from './anthology-card/anthology-card.component';
-import { HomeComponent } from './home/home.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {AppRouterModule} from './app.router.module';
+import {TopNavigationComponent} from './top-navigation/top-navigation.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+import {ArticleCardComponent} from './article-card/article-card.component';
+import {AuthorCardComponent} from './author-card/author-card.component';
+import {AnthologyCardComponent} from './anthology-card/anthology-card.component';
+import {HomeComponent} from './home/home.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +20,15 @@ import { HomeComponent } from './home/home.component';
     ArticleCardComponent,
     AuthorCardComponent,
     AnthologyCardComponent,
-    HomeComponent
+    HomeComponent,
+    LogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- root/web/src/app/app.router.module.ts
+++ root/web/src/app/app.router.module.ts
@@ -1,11 +1,39 @@
-import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {Route, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+
+const routes: Route[] = [
+  {
+    path: 'login',
+    component: LoginFormComponent
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
-export class RouterModule {
+export class AppRouterModule {
 }
