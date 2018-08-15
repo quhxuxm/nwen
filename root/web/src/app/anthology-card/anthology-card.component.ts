43da4d0842747ce1ad293c583d83bfa839c48c5a
quh <quhxuxm_quh@163.com>
2018-08-15 17:21:04 +0800

    init

---
+++ root/web/src/app/anthology-card/anthology-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anthology-card',
+  templateUrl: './anthology-card.component.html',
+  styleUrls: ['./anthology-card.component.scss']
+})
+export class AnthologyCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- root/web/src/app/app.module.ts
+++ root/web/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { ArticleCardComponent } from './article-card/article-card.component';
+import { AuthorCardComponent } from './author-card/author-card.component';
+import { AnthologyCardComponent } from './anthology-card/anthology-card.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    ArticleCardComponent,
+    AuthorCardComponent,
+    AnthologyCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
