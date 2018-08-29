0867d48f07a0765bdf71b49dbba40ab6c8ee7fd2
Qu Hao <quhxuxm_quh@163.com>
2018-08-29 22:47:58 +0800

    init

--- root/server/entry/src/main/java/online/nwen/entry/HomeController.java
+++ root/server/entry/src/main/java/online/nwen/entry/HomeController.java
@@ -1,9 +1,11 @@
 package online.nwen.entry;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class HomeController {
     @RequestMapping("/hello")
     public String hello() {
