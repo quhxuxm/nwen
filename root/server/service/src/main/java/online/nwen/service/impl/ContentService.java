c1ce65c1d6abf6f8a896b190b676eb5ede22b2b0
Qu Hao <quhxuxm_quh@163.com>
2018-09-14 07:37:40 +0800

    init

--- root/server/service/src/main/java/online/nwen/service/impl/ContentService.java
+++ root/server/service/src/main/java/online/nwen/service/impl/ContentService.java
@@ -1,12 +1,19 @@
 package online.nwen.service.impl;
 
 import online.nwen.service.api.IContentService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 @Service
 class ContentService implements IContentService {
     @Override
     public String parse(String content) {
+        Document contentDocument = Jsoup.parse(content);
+        Elements imgElements = contentDocument.getElementsByTag("img");
+        Elements audioElements = contentDocument.getElementsByTag("audio");
+        Elements vadioElements = contentDocument.getElementsByTag("vadio");
         return content;
     }
 }
