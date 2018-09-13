b3bc7eeeb11e9ceef20e81b11c108d1c1e6eb73b
quh <quhxuxm_quh@163.com>
2018-09-13 16:10:29 +0800

    init

--- root/server/service/src/main/java/online/nwen/service/api/IArticleService.java
+++ root/server/service/src/main/java/online/nwen/service/api/IArticleService.java
@@ -1,12 +1,11 @@
 package online.nwen.service.api;
 
-import online.nwen.service.dto.article.PraiseArticleDTO;
-import online.nwen.service.dto.article.PraiseArticleResultDTO;
-import online.nwen.service.dto.article.SaveArticleDTO;
-import online.nwen.service.dto.article.SaveArticleResultDTO;
+import online.nwen.service.dto.article.*;
 
 public interface IArticleService {
     SaveArticleResultDTO saveArticle(SaveArticleDTO saveArticleDTO);
 
     PraiseArticleResultDTO praiseArticle(PraiseArticleDTO praiseArticleDTO);
+
+    BookmarkArticleResultDTO bookmarkArticle(BookmarkArticleDTO bookmarkArticleDTO);
 }
